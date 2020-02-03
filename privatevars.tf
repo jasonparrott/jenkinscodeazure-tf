5c40a51109f4cbc86cb40e04a4b230b81e3c84ab
Jason Parrott <jason.parrott@gmail.com>
2020-02-03 17:19:59 +0000

    Split out privatevars

---
+++ privatevars.tf
@@ -0,0 +1,7 @@
+variable "client_secret" { default = "" }
+variable "subscription_id" { default = "" }
+variable "client_id" { default = "" }
+variable "tenant_id" { default =  "" }
+variable "app_id" { default = "" }
+variable "app_token" { default = "" }
+
